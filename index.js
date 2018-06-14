6ae990ae37e28ff09d9d5adf56552e036a5e9a9b
Nicolas Botello <nbotell2@gmail.com>
2018-06-13 23:14:57 -0700

    add more log statements

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const rp = require('request-promise')
 const dotenv = require("dotenv");
-
 dotenv.config();
 //env varibles
 const clientId = process.env.ANILIST_CLIENT_ID;
@@ -10,16 +9,19 @@ const clientSecret = process.env.ANILIST_CLIENT_SECRET;
 const redirectUri = process.env.ANILIST_REDIRECT_URI;
 const PORT = process.env.PORT || 5000;
 
+console.log('This is the clientId %s', clientId);
+
 express()
   .use(express.static(path.join(__dirname, "public")))
   .get("/", (req, res) => res.render("public/index"))
   .get("/callback", (req, res) => {
       const code = req.query.code;
-      function handleError(res){
+      function handleError(res, err){
+          console.log("This was the error %O", err);
           return res.json({ "error": "There was an error" });
       }
         if(!code){
-            handleError(res);
+            handleError(res, {"message": "there was no code"});
         }
 
         const options = { 
@@ -40,10 +42,10 @@ express()
                    acces_token : body.access_token
                });
             } else {
-                handleError(res)
+                handleError(res, body)
             }
         })
-          .catch(() => handleError(res))
+          .catch(() => handleError(res, err))
         
     })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
